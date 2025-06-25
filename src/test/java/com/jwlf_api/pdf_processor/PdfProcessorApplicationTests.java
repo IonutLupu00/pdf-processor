b9031af070603d9f298254bea528509785c25528
Ionut Lupu <ionutlupu5000@gmail.com>
2025-06-25 20:33:42 +0300

    Initial commit

---
+++ src/test/java/com/jwlf_api/pdf_processor/PdfProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwlf_api.pdf_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
